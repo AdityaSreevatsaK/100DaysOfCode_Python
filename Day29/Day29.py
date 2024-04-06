54e2172f6aa60efc4300a3df2cfd36511df0f31a
Aditya Sreevatsa K <105434583+Aditya-SreevatsaK@users.noreply.github.com>
2024-04-06 13:50:14 +0530

    [minor] 100 Days of Code - Day 29.

--- Day29/Day29.py
+++ Day29/Day29.py
@@ -1,4 +1,8 @@
+from random import choice, randint, shuffle
 from tkinter import *
+from tkinter import messagebox
+
+import pyperclip
 
 print("Day 29 - 100 Days of Code.")
 print("Welcome to Password Manager.")
@@ -6,7 +10,9 @@ print("Welcome to Password Manager.")
 ZERO = 0
 ONE = 1
 TWO = 2
-BACKGROUND_COLOUR = "#FFF6E9"
+THREE = 3
+EMPTY_STRING = ""
+BACKGROUND_COLOUR = "#B3C8CF"
 font_tuple = ("Palatino Linotype", 10, "bold")
 
 
@@ -14,10 +20,57 @@ def save_data_after_validation():
     """
         Method to perform validations on the fields and save the same into the file.
     """
-    print("Okay")
-    print("website_entry", str(website_entry), type(str(website_entry)))
-    print("email_username_entry", str(email_username_entry))
-    print("password_entry", str(password_entry))
+    website = website_entry.get().title()
+    email = email_username_entry.get().lower()
+    password = password_entry.get()
+    pipe_symbol = " | "
+    if website == EMPTY_STRING:
+        messagebox.showerror(title="Blank field(s).", message="Website field is empty.")
+        return
+    elif password == EMPTY_STRING:
+        messagebox.showerror(title="Blank field(s).", message="Password field is empty.")
+        return
+    elif email == EMPTY_STRING:
+        messagebox.showerror(title="Blank field(s).", message="Email field is empty.")
+        return
+    elif " " in password:
+        messagebox.showerror(title="Invalid entry.", message="Password field has a whitespace.")
+        return
+    else:
+        pass
+
+    confirm_addition = messagebox.askyesno(title="Are these values correct?", message=f"Website: {website}\n"
+                                                                                      f"Email ID / Username: {email}\n"
+                                                                                      f"Password: {password}")
+    if confirm_addition:
+        try:
+            with open(file="Passwords.txt", mode="a") as passwords_file:
+                passwords_file.write(website + pipe_symbol + email + pipe_symbol + password + pipe_symbol + "\n")
+        except FileNotFoundError:
+            print("Exception encountered.")
+        else:
+            messagebox.showinfo(title="Added.", message="Addition successful.")
+    else:
+        pass
+
+
+def generate_randomised_password():
+    """
+        Method to generate a random 12 character password.
+    """
+    randomised_password = []
+    password_length = 12
+    special_characters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", "_"]
+    for iterator in range(int(password_length / 4)):
+        randomised_password.append(choice(special_characters))
+        randomised_password.append(chr(randint(65, 90)))
+        randomised_password.append(str(randint(ZERO, 9)))
+        randomised_password.append(chr(randint(97, 122)))
+    shuffle(randomised_password)
+    password_entry.delete(ZERO, 13)
+    password = EMPTY_STRING.join([character for character in randomised_password])
+    pyperclip.copy(password)
+    password_entry.insert(ZERO, password)
 
 
 window = Tk()
@@ -41,17 +94,17 @@ email_username_field_name = Label(text="Email ID/ Username:", font=font_tuple,
 email_username_field_name.grid(column=ZERO, row=TWO)
 
 email_username_entry = Entry(width=52)
-email_username_entry.insert(0, "ASK@email.com")
+email_username_entry.insert(ZERO, "ASK@email.com")
 email_username_entry.grid(column=ONE, row=TWO, columnspan=TWO)
 
 password_field_name = Label(text="Password:", font=font_tuple, bg=BACKGROUND_COLOUR, highlightthickness=ZERO)
-password_field_name.grid(column=ZERO, row=3)
+password_field_name.grid(column=ZERO, row=THREE)
 
 password_entry = Entry(width=32)
-password_entry.grid(column=ONE, row=3)
+password_entry.grid(column=ONE, row=THREE)
 
-generate_password_button = Button(text="Generate Password.")
-generate_password_button.grid(column=TWO, row=3)
+generate_password_button = Button(text="Generate Password.", command=generate_randomised_password)
+generate_password_button.grid(column=TWO, row=THREE)
 
 add_button = Button(text="Add.", width=45, command=save_data_after_validation)
 add_button.grid(column=ONE, row=4, columnspan=TWO)
